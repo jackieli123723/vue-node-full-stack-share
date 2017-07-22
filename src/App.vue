124eddcbeec45d162b1254cb87e5f8458e8e4a3e
jackieli123723 <380863274@qq.com>
2017-07-22 19:54:45 +0800

    :rocket::fire:完成ppt

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="reveal">
 			<div class="slides">
-				<section>
+
+				<section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2405.PNG">
           <div >NodeJS的全栈式开发</div><br/>
           <div class="fon14">作者: 李立冬 (共享平台部web前端工程师)</div> <br/> 
           <div>
@@ -10,29 +11,132 @@
             <a href="http://lilidong.cn" title="web" target="_blank" class="icon-web icon-btn"></a>
           </div>
         </section>
-        <section data-background="https://media.giphy.com/media/Bh3YfliwBZNwk/giphy.gif">
-          <section>
-          <div>全栈工程师</div>
-          <div><img src="./assets/node.jpg" height="500" /></div>
-          </section>
-          <section style="text-align:left;padding:0 20px;line-height:1.5">
-          <em class="color2">Node.js</em>是 <em class="color2">JavaScript</em> 后端开发语言。从诞生之初就备受关注，到如今说到最火的后端 Web 开发，Node 说自己是第二，没有人敢说他是第一。正是 Node 的兴起，还带动了前端 JS 的热度，<i class="color3">react.js</i> <i class="color4">vue.js</i> 这些前端 JS框架 也火借东风，烧的很旺，甚至连“全栈”这个词也成为互联网招聘的热搜关键字，从此，JavaScript 程序员不再是被局限于浏览器的前端开发者了
-          </section>
-        </section>
 
-				<section>
-   <!--  <section>{{ 'Vertical Slide 1' | date('...') }}</section> -->
-			    <section>前后端都采用JavaScript的好处</section>
-          <section>1.无需切换语言环境，JavaScript可以做到后端语言能做的绝大部分事情</section>
-          <section>2.数据可以很好的实现跨前后端使用（使用json）</section>
-          <section>3.一些业务(如模板渲染)不需要纠结在前端还是后端处理</section>
-          <page-item></page-item>
+        <section>
+          <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2365.JPG">
+            <div><em class="color2 " style="font-size:2em">Node.js</em></div><br/>
+            <ul class="fon16" style="font-size:0.8em;line-height:2">
+              <li>是一个基于 <em class="color7">Chrome V8</em> 引擎的 <em class="color1">JavaScript</em> 运行环境。</li> 
+              <li>使用了一个事件驱动、非阻塞式 <em class="color5">I/O</em> 的模型，使其轻量又高效。 </li> 
+              <li>管理器 <em class="color3">npm</em>，是全球最大的开源库生态系统。</li> 
+            </ul>
+          </section>
+
+          <section style="text-align:left;padding:0 20px;line-height:1.5" data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2406.PNG">
+             概括来说,<em class="color2">Node.js</em>是<em class="color7">JavaScript</em>后端开发语言。正是 <em class="color2">Node.js</em> 的兴起，还带动了前端  的热度，从诞生之初就备受关注，到如今<em class="color2">Node.js</em>被各大互联网公司应用到各自的产品中。从此，<em class="color7">JavaScript</em> 程序员不再是被局限于浏览器的前端开发者了。
+          </section>
+
         </section>
 
         <section>
+                <section data-background="http://otha2y1bi.bkt.clouddn.com/bg1.jpg"> 
+                  <div style="font-size:1.5em">传统<em class="color2">WEB</em>应用开发的混沌时代</div>
+                </section>
+
+                <section style="text-align:left;padding:0 20px;line-height:1.5"  data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2393.JPG">
+                  在传统的像<em class="color2">JSP,PHP</em>等开发模式中，前端处在一个混沌的状态，前端负责切图和编写静态页面,后端负责将数据渲染到静态页面，最后页面在通过浏览器渲染展示，这个过程中前端只是写了一些简单的样式和基础<em class="color2">javascript</em>脚本，数据渲染的过程的j脚本甚至是后端程序编写，导致这样的情况就是前后端的运行环境不统一带来的。
+                </section>
+        </section>
+
+        <section data-transition="fade" data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2361.PNG">
+          <div style="font-size:1.8em">传统web开发存在的<em class="color2">缺点</em></div><br/>
+          <ul class="fon16" style="font-size:1em;line-height:2">
+            <li>代码<em class="color4">耦合</em>性差</li> 
+            <li>公共代码抽离<em class="color1">模块</em>化难度大</li> 
+            <li><em class="color7">业务逻辑</em>代码的分离界限不明确</li> 
+          </ul>
+        </section>
+
+        <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2389.PNG">
+              <div style="font-size:1.5em">如何解决这些<em class="color2">缺点</em>？</div>
+        </section>
+
+
+        <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2416.JPG">
+              <div style="font-size:1.5em"><em class="color2">Node.js</em>打通前后端的运行环境</div>
+        </section>
+
+        <section>
+            <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2364.JPG">
+               <div style="font-size:2em"><em class="color2">路由</em>的概念</div><br/>
+            <!--    <div class="fon16" style="font-size:0.8em;line-height:2;text-align:left">  路由是指如何定义应用的端点（URIs）以及如何响应客户端的请求。</div> -->
+            </section>
+
+            <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2395.JPG">
+                <div style="font-size:1.8em">传统后端路由</div><br/>
+                <ul class="fon16" style="font-size:1em;line-height:2">
+                  <li>每跳转到不同的URL，都是重新访问服务端，然后服务端返回页面，页面也可以是服务端获取数据，然后和模板组合，返回HTML，也可以是直接返回模板HTML，然后由前端js再去请求数据，使用前端模板和数据进行组合，生成想要的HTML。</li>  
+                </ul>
+            </section>
+
+            <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2390.JPG">
+                <div style="font-size:1.8em">前端路由(MVC,MVVM框架)</div><br/>
+                <ul class="fon16" style="font-size:1em;line-height:1.8;">
+                  <li>每跳转到不同的URL都是使用<em class="color2">hash/history</em>模式的路由分发控制页面之间的跳转，实际上只是JS根据URL来操作DOM元素，根据每个页面需要的去服务端请求数据，返回数据后和模板进行组合，当然模板有可能是请求服务端返回的，这就是 <em class="color2">SPA </em>单页程序。</li>  
+                </ul>
+            </section>
+
+        </section>
+
+        <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2413.PNG">
+           <div style="font-size:1.5em">如何利用<em class="color2">NODE</em>来实现后端的路由</div>
+        </section>
+
+        <section>
+              <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2403%281%29.JPG">
+                   <div style="font-size:1.8em"><em class="color2">Express</em></div><br/>
+                    <ul class="fon16" style="font-size:1em;line-height:1.5">
+                      <li>Express 是一个基于 Node.js 平台的极简、灵活的 web 应用开发框架，它提供一系列强大的特性，帮助你创建各种 Web 和移动设备应用。</li> 
+                      <li>丰富的 HTTP 快捷方法和任意排列组合的 Connect 中间件</li> 
+                      <li>express生态扩展了 Web 应用所需的基本功能轮子。</li> 
+                    </ul>
+              </section>
+              <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2399%281%29.PNG">
+                 <pre><code style="background:#9d9696">$ npm install express --save</code></pre>
+                <img src="http://otha2y1bi.bkt.clouddn.com/express.png"/>
+              </section>
+
+              <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2399%281%29.PNG">
+                 <pre><code style="background:#9d9696">$ server.js</code></pre>
+                <img src="http://otha2y1bi.bkt.clouddn.com/QQ%E6%88%AA%E5%9B%BE20170722184634.png"/>
+              </section>
+
+              <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2399%281%29.PNG">
+                 <pre><code style="background:#9d9696">$ route/index.js</code></pre>
+                <img src="http://otha2y1bi.bkt.clouddn.com/QQ%E6%88%AA%E5%9B%BE20170722184940.png"/>
+              </section>
+
+        </section>
+
+        <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2362.PNG">
+           <div style="font-size:1.5em">前后端都采用<em class="color2">JavaScript</em>来编写业务逻辑代码</div>
+        </section>
+
+
+        <section>
+          <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2360.JPG"> 
+            <div style="font-size:1.5em">前后端分离的<em class="color2">Node</em>时代</div>
+          </section>
+          <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2366.JPG">
+            <ul class="fon16" style="font-size:1em;line-height:2">
+              <li>无需切换语言环境，<i class="color3">JavaScript</i>可以做到后端语言能做的绝大部分事情</li> 
+              <li>数据可以很好的实现跨前后端使用（使用<i class="color3">json</i>） </li> 
+              <li>一些业务(如模板渲染)不需要纠结在前端还是后端处理</li> 
+            </ul>
+          </section>
+          
+        </section>
+
+
+        <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2418.JPG">
+                <div style="font-size:1.5em">前端工程师不再是<em class="color2">“切图仔”</em>，也可以玩NODE全栈开发</div>
+        </section>
+
+        
+         <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2357.PNG">
             <hgroup>
-            <img src="./assets/bg1.jpg"  height="300" />
-             <h2 class="color5">node的全栈技术栈清单</h2>
+  
+             <h2 class="color5"><em class="color2">Node</em>的全栈技术栈清单</h2>
             </hgroup>
             <article>
             <ul>
@@ -44,20 +148,25 @@
             </article>
         </section>
 
-        <section data-transition="fade">
+        
+        <section data-transition="fade" data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2407.PNG">
           <h2>谢谢观赏</h2>
           <img src="https://media.giphy.com/media/2gGEWrIGVioP6/giphy.gif" class="dubblesize">
         </section>
+
+        <section data-background="http://otha2y1bi.bkt.clouddn.com/IMG_2400%281%29.PNG">
+            <h2>没太懂讲的什么？我们来个龙渊道具商城后台来演示</h2>
+            <a class="color2" href="http://mall.lilidong.cn/" target="_blank">猛戳进入</a>
+        </section>
  
        
-
 			</div>
 		</div>
 </template>
 
 <script>
 
-import pageItem from './components/page-item'
+
 import reveal from 'reveal.js'
 import socket from 'socket.io-client'
 
@@ -66,7 +175,7 @@ let io = socket('http://192.168.0.175/:3000')
 export default {
   name: 'app',
   components: {
-    pageItem
+
   },
   data () {
     return {
